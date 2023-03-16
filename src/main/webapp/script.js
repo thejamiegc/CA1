87c006aa88a14fb88dc621a4926f90d5f49b6f72
Helena Lykstoft <helena.botn.lykstoft@gmail.com>
2023-03-16 12:04:40 +0100

    Adds cors folder and changes to frontend

--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -5,14 +5,60 @@ const more = document.querySelector("#more")
 const evenmore = document.querySelector("#evenmore")
 
 
-const msg = document.querySelector("#msg")
+// PEOPLE
+let people = [];
 
-function displayMessage(msg){
-    document.querySelector("#msg").textContent = msg;
+function displayPeople(){
+    // let string = "";
+    // for(let i=0;i<people.length;i++) {
+    //     string += people.get(i) + "/n";
+    // }
+    // console.log(string)
+    document.getElementById("msg").innerHTML = people.join(" <br> ");
+
 }
 
-getall.addEventListener('click',event)=>{
+function addPerson(data){
+
+    for(let i=0;i<data.length;i++) {
+        const person = {
+            email: data[i].email,
+            firstname: data[i].firstname,
+            gender: data[i].gender,
+            lastname: data[i].lastname,
+            relationshipStatus: data[i].relationshipStatus,
+
+            toString() {
+                return `${this.firstname} ${this.lastname} ${this.email} ${this.gender} ${this.relationshipStatus}`
+            }
+        }
+        people[i] = person
+    }
+    console.log("efter loop" + people)
+}
+
+
+getall.addEventListener('click', (event)=>{
+    console.log("CLICKED on getall")
     fetch(`http://localhost:8080/ca1/api/person`)
         .then(response=>response.json())
-        .then(data =>)
-}
+        .then(data=>addPerson(data))
+        .then(displayPeople)
+        .catch(error=>console.log("An error has occured."))
+})
+
+
+// HOBBY
+hobbysearch.addEventListener('click', (event)=>{
+    console.log("CLICKED on hobbysearch")
+})
+
+// MORE
+more.addEventListener('click', (event)=>{
+    console.log("CLICKED on more")
+})
+
+// EVEN MORE
+evenmore.addEventListener('click', (event)=>{
+    console.log("CLICKED on EVEN MORE!")
+})
